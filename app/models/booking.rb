bebcca34cade9f6da404a7cd94c346036b67ca74
Nicole Wong <nrewong@gmail.com>
2020-05-18 17:35:56 +0900

    add date overlapping validation for bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,10 +4,22 @@ class Booking < ApplicationRecord
 
   validates :start_date, :end_date, presence: true
   validate :dates_valid
+  validate :availability
+
+
 
   private
   def dates_valid
     return unless start_date && end_date
     errors.add(:end_date, "start date must be before end date") if end_date <= start_date
   end
+
+   def availability
+    # exclude end date in range because you can start on the same day another booking ends
+    other_bookings_ranges = space.bookings.map { |b| (b.start_date...b.end_date) }
+    if other_bookings_ranges.any? { |range| range.overlaps?(start_date...end_date) }
+      errors.add(:start_date, "dates overlap with another booking")
+    end
+  end
+
 end
