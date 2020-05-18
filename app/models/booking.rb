73b7324874af89b9444c4903350ec646614365fd
Nicole Wong <nrewong@gmail.com>
2020-05-18 17:05:02 +0900

    added validation for start date being after end date

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,13 @@
 class Booking < ApplicationRecord
   belongs_to :space
   belongs_to :user
+
   validates :start_date, :end_date, presence: true
+  validate :dates_valid
+
+  private
+  def dates_valid
+    return unless start_date && end_date
+    errors.add(:end_date, "start date must be before end date") if end_date <= start_date
+  end
 end
