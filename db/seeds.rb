5de39790499ddbfa19e3b4761881cb5f184259a0
Nicole Wong <nrewong@gmail.com>
2020-05-18 16:03:34 +0900

    create user, space, and booking models plus seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Destroying all users"
+User.destroy_all
+
+puts 'About to create 5 users'
+
+5.times do
+  user = User.new
+  user.email = Faker::Internet.free_email
+  user.password = 'password'
+  user.password_confirmation = 'password'
+  user.save!
+end
+
+puts 'Created 5 users'
+
+
+puts 'About to create 20 spaces'
+
+users = User.all
+
+adjectives = %w(Spacious Wide Small Big Medium Tiny Convenient Cool Superb Great Nice)
+
+20.times do
+  Space.create!(
+    title: "#{adjectives.sample} #{Faker::House.room}",
+    address: Faker::Address.full_address,
+    user: users.sample,
+    description: Faker::Lorem.sentences(number: 8).join(' '),
+  )
+end
+
+
+puts 'Created 20 spaces.'
