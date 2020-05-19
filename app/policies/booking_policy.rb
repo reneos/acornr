1920d99705613ef1b5ed2100372995af1b7e168b
Nicole Wong <nrewong@gmail.com>
2020-05-19 11:17:25 +0900

    create booking policy

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -4,4 +4,14 @@ class BookingPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def show?
+    # can view a booking if you are the host or the renter
+    record.space.user == user || record.user == user
+  end
+
+  def create?
+    # Can only create if the space does not belong to current user
+    record.space.user != user
+  end
 end
