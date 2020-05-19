689c7678e8e59eedf57fc6b5cc0a5d56ee0020ed
Lukasz <lukaszkkoziel@gmail.com>
2020-05-19 11:26:40 +0900

    creating booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,26 @@
 class BookingsController < ApplicationController
+  before_action :set_space, only: [:new, :create]
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.space = @space
+    if @booking.save
+      redirect_to space_path(@space)
+    else
+      render 'bookings/show'
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :user, :space)
+  end
+
+  def set_space
+    @space = Space.find(params[:space_id])
+  end
 end
