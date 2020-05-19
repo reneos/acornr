dc9b47ded7f31eb2200e86c3fa8ef83394f61ecd
Lukasz <lukaszkkoziel@gmail.com>
2020-05-19 14:39:35 +0900

    price added to spaces and validations

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,5 +1,5 @@
 class Space < ApplicationRecord
   belongs_to :user
   has_many :bookings
-  validates :title, :address, :description, presence: true
+  validates :title, :address, :description, presence:, :price true
 end
