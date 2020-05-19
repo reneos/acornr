10f301f8af80d708c3d4abf1c6294b60a0c97d4f
Momotaro Nakamura <mrnakamura721@gmail.com>
2020-05-19 11:00:29 +0900

    Completed spaces controller

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,2 +1,29 @@
 class SpacesController < ApplicationController
+  def index
+    @spaces = Space.all
+  end
+
+  def show
+    @space = Space.find(params[:id])
+  end
+
+  def new
+    @space = Space.new
+  end
+
+  def create
+    @space = Space.new(space_params)
+    if @space.save
+      redirect_to @space, notice: 'Space was successfully created.'
+    else
+      render :new
+    end
+
+  end
+
+  private
+
+  def space_params
+    params.require(:space).permit(:title, :description, :address)
+  end
 end
