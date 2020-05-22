a412620f84b59a09c54359abf9179cf0531d4516
Nicole Wong <nrewong@gmail.com>
2020-05-22 16:25:00 +0900

    tweak layout

--- app/javascript/plugins/init_litepicker.js
+++ app/javascript/plugins/init_litepicker.js
@@ -3,10 +3,12 @@ const calculate_price = (date1, date2) => {
   const price = parseInt(document.querySelector('.bookings-form').dataset.price, 10);
   const days = (date2.getTime() - date1.getTime()) / (1000 * 3600 * 24);
   const totalPriceBox = document.querySelector('#total_price');
-  const content = `<span class="emphasize">Total:</span> ¥${price * days} for ${days} days`;
+  const counter = days == 1 ? "day" : "days";
+  const content = `<span class="emphasize">Total:</span> ¥${price * days} for ${days} ${counter}`;
   totalPriceBox.innerHTML = content;
   totalPriceBox.style.display = "block"
 }
+
 
 const init_litepicker = () => {
   const startDateInput = document.querySelector('#booking_start_date');
